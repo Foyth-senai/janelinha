5022d44ccfb0c969759748cfd97a18c4ee9ff2c5
Foyth-senai <124690153+Foyth-senai@users.noreply.github.com>
2023-05-04 07:53:51 -0300

    asdfasfda

---
+++ script.js
@@ -0,0 +1,10 @@
+function Mostrar(){
+    texto = email.value;
+    alert(texto);
+}
+
+function MostrarJanelinha(){
+    texto = email.value;
+    txt.innerHTML = texto;
+    janelinha.style.display = "block";
+}
